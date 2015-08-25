7ac85418bea83a6cec082dcc38c32d3022d9c1ab
Joost de Vries <joost@ziener.org>
2015-08-25 08:20:17 +0200

    adds typescript compilation

---
+++ play/app/assets/javascripts/main.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by joost1 on 24/08/15.
+ */
+class Student {
+    fullname : string;
+    constructor(public firstname, public middleinitial, public lastname) {
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+}
+
+interface Person {
+    firstname: string;
+    lastname: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = new Student("Jane", "M.", "User");
